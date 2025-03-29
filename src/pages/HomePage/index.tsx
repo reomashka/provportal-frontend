680e03cd16a16f835afe3f1f4349994545d6ad0f
romashka <romankuvaev820@gmail.com>
2025-03-29 12:00:17 +0300

    redesign transportInfo

--- src/pages/HomePage/index.tsx
+++ src/pages/HomePage/index.tsx
@@ -7,6 +7,18 @@ export const HomePage: React.FC = () => {
   return (
     <div className='container'>
       <HomePageMetadata />
+      {/* Yandex.RTB R-A-14786500-1 */}
+      <div id='yandex_rtb_R-A-14786500-1'></div>
+      <script>
+        {`
+          window.yaContextCb.push(() => {
+            Ya.Context.AdvManager.render({
+              "blockId": "R-A-14786500-1",
+              "renderTo": "yandex_rtb_R-A-14786500-1"
+            })
+          })
+        `}
+      </script>
       <HomeModule />
       <Footer />
     </div>
